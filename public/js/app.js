c0494d336118b15d2de47928f5a13ff21ff66323
unknown <valegjorgjieska@gmail.com>
2021-02-03 11:12:38 +0100

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,43 @@
+console.log('Client side javascript file is loaded!');
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+const messageThree = document.querySelector('#message-3');
+const messageFour = document.querySelector('#message-4');
+
+
+weatherForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const location = search.value;
+
+    messageOne.textContent='Loading ...';
+    messageTwo.textContent = '';
+    messageThree.textContent = '';
+    messageFour.textContent = '';
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response)=>{
+    response.json().then((data)=>{
+        if(data.error) {
+            messageOne.textContent=data.error;
+            messageTwo.textContent = '';
+            messageThree.textContent = '';
+            messageFour.textContent = '';
+            //console.log(data.error);
+        } else {
+            messageOne.textContent = data.location;
+            messageTwo.textContent = data.forecast;
+            messageThree.textContent = data.temperature;
+            messageFour.textContent = data.feelslike;
+
+            //console.log(data.address);
+            //console.log(data.location);
+            //console.log(data.forecast);
+            //console.log(data.temperature);
+            //console.log(data.feelslike);            
+        }
+    });
+});
+    
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, units, callback) => {    
+    const url='http://api.weatherstack.com/current?access_key=64da51f1546599415f1b3879ced5ab41&query='+ latitude +',' + longitude + '&units='+ units;
+    request({url,json:true},(error,{body})=>{
+        
+        if(error) {
+            callback('Unable to connect to the weather service!',undefined);
+        } else if (body.error) {
+            callback('Unable to find the location!',undefined);
+        } else {
+            callback(undefined,{
+                weatherDescription: body.current.weather_descriptions[0],
+                currentTemperature: body.current.temperature,
+                feelslike:body.current.feelslike
+            })
+        }
+    });
+}
+
+module.exports = forecast;
